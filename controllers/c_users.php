09ac0178fafb93f46be7e2bdd264eb632140dedd
Kishleen Saini <kishleensaini@gmail.com>
2013-11-05 21:11:16 -0500

    Profile page changed

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -41,23 +41,6 @@ class users_controller extends base_controller {
 		$this->template->content = View::instance('v_users_profile');
 		$this->template->title = "Profile";
 		
-		# Load client files
-		$client_files_head = Array (
-			' /css/profile.css', 
-			);
-		
-		$this->template->client_files_head = Utils::load_client_files
-
-($client_files_head);
-		
-		$client_files_body = Array (
-			' /js/profile.js'
-			);
-		
-		$this->template->client_files_body = Utils::load_client_files
-
-($client_files_body);
-		
 		#Pass the data to the view
 		$this->template->content->user_name = $user_name;
 		
@@ -67,6 +50,5 @@ class users_controller extends base_controller {
 		//$view->user_name = $user_name;
         //echo $view;
     }
-
 
 } # end of the class
